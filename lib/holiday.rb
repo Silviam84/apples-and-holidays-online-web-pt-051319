50c436ecea95988ee29c43eaf3751a0abfc00856
Silvia Mendoza <silviamendoza223@yahoo.com>
2019-05-26 19:10:25 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -4,14 +4,14 @@ def second_supply_for_fourth_of_july(holiday_hash)
   # given that holiday_hash looks like this:
   def second_supply_for_fourth_of_july(holiday_supplies)
     
-    holiday_supplies 
+    holiday_supplies [:summer] [:fourth_of_july][1]
   # {
   #   :winter => {
   #     :christmas => ["Lights", "Wreath"],
   #     :new_years => ["Party Hats"]
   #   },
   #   :summer => {
-  #     :fourth_of_july => ["Fire[works", "BBQ"]
+  #     :fourth_of_july => ["Fireworks", "BBQ"]
   #   },
   #   :fall => {
   #     :thanksgiving => ["Turkey"]
